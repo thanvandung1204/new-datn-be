d4d7823b2934a5740ac022b8d76741c3fd90c34f
tiendat6782 <tiendat56782@gmail.com>
2024-07-02 19:50:07 +0700

    update route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,16 @@
 import AuthRouter from './auth.router.js';
+import sizeRoutes from './size.routes.js';
+import toppingRoutes from './topping.routes.js';
 import uploadRouter from './uploadfiles.routes.js';
 import UserRoutes from './user.routers.js';
+import productRoutes from './product.routes.js';
+import categoryRoutes from './category.routes.js';
+import cartRouter from './cart.routes.js';
+import orderRoutes from './order.routes.js';
 import express from 'express';
+import uploadBanner from './banner.routes.js';
 const router = express.Router();
-const rootRoutes = [UserRoutes, AuthRouter, uploadRouter];
+const rootRoutes = [UserRoutes, AuthRouter, uploadRouter, sizeRoutes, toppingRoutes,productRoutes,categoryRoutes,orderRoutes,cartRouter,uploadBanner];
 rootRoutes.map((route) => {
   router.use(route);
 });
