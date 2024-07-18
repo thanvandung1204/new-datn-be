baa32e6c248f30e815fb4e674ae9c191454bd4d2
mittoHieu <117790527+mittoHieu@users.noreply.github.com>
2024-07-18 19:03:06 +0700

    categoryblog

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,18 +5,18 @@ import uploadRouter from './uploadfiles.routes.js';
 import UserRoutes from './user.routers.js';
 import productRoutes from './product.routes.js';
 import categoryRoutes from './category.routes.js';
-
 import cartRouter from './cart.routes.js';
 import orderRoutes from './order.routes.js';
-
 import express from 'express';
 import uploadBanner from './banner.routes.js';
 import voucherRoutes from './voucher.routes.js';
+import newBlogRouter from './newsBlogs.routes.js';
+import categoryBlogRoutes from './category-blog.routes.js';
+import analyticRoutes from './analytic.routes.js';
+import vnpayRoutes from './vnpay.rotues.js';
+import addressRouter from './address.routes.js';
 
 const router = express.Router();
-
-
-
 const rootRoutes = [
   UserRoutes,
   AuthRouter,
@@ -29,8 +29,12 @@ const rootRoutes = [
   cartRouter,
   uploadBanner,
   voucherRoutes,
+  newBlogRouter,
+  categoryBlogRoutes,
+  analyticRoutes,
+  vnpayRoutes,
+  addressRouter
 ];
-
 rootRoutes.map((route) => {
   router.use(route);
 });
