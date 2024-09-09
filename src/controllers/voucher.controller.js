0a11243541d4ed1a8019f439153fbe164d8554c7
quanlmph18475 <quanlmph18475@fpt.edu.vn>
2024-09-10 00:00:12 +0700

    umaxuser-voucher

--- src/controllers/voucher.controller.js
+++ src/controllers/voucher.controller.js
@@ -1,28 +1,23 @@
-import Voucher from '../models/voucher.model.js';
-import { voucherValidate } from '../validates/voucher.validate.js';
+import Voucher from "../models/voucher.model.js";
+import { voucherValidate } from "../validates/voucher.validate.js";
 
 export const voucherController = {
   /* create */
   create: async (req, res) => {
     try {
       const body = req.body;
-
-      /* validate */
       const { error } = voucherValidate.validate(body, { abortEarly: false });
 
       if (error) {
         const errors = error.details.map((err) => err.message);
         return res.status(400).json({ message: errors });
       }
-      //  check code đã tồn tại hay chưa
 
-      const isCode = await Voucher.findOne({ code: body.code })
-       
+      const isCode = await Voucher.findOne({ code: body.code });
       if (isCode) {
-
         return res.status(400).json({ message: "Mã code đã tồn tại rồi!" });
       }
-      /* create */
+
       const voucher = await Voucher.create({ ...body });
       if (voucher) {
         return res.status(201).json({ data: voucher });
@@ -40,7 +35,7 @@ export const voucherController = {
         limit: _limit,
         sort: { createdAt: -1 },
       };
-      const query = q ? { code: { $regex: new RegExp(q), $options: 'i' } } : {};
+      const query = q ? { code: { $regex: new RegExp(q), $options: "i" } } : {};
       const vouchers = await Voucher.paginate(query, options);
       return res.status(200).json({ data: vouchers });
     } catch (error) {
@@ -57,7 +52,7 @@ export const voucherController = {
         sort: { createdAt: -1 },
       };
       const query = q
-        ? { code: { $regex: new RegExp(q), $options: 'i' }, isActive: true }
+        ? { code: { $regex: new RegExp(q), $options: "i" }, isActive: true }
         : { isActive: true };
       /* lấy ra thời gian hiện tại để so sánh */
       const currentDate = new Date();
@@ -84,32 +79,42 @@ export const voucherController = {
   },
   /* update voucher */
   update: async (req, res) => {
-    try {
-      const { id } = req.params;
-      const body = req.body;
-      /* validate */
-      const { error } = voucherValidate.validate(body, { abortEarly: false });
-      if (error) {
-        const errors = error.details.map((err) => err.message);
-        return res.status(400).json({ message: errors });
-      }
-      /* update */
-      const voucher = await Voucher.findByIdAndUpdate(id, body, { new: true });
-      if (!voucher) {
-        return res.status(404).json({ message: 'Không tìm thấy Voucher  ' });
-      }
-      return res.status(200).json({ data: voucher });
-    } catch (error) {
-      return res.status(500).json({ message: error.message });
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const { error } = voucherValidate.validate(body, { abortEarly: false });
+
+    if (error) {
+      const errors = error.details.map((err) => err.message);
+      return res.status(400).json({ message: errors });
     }
-  },
+
+    const voucher = await Voucher.findById(id);
+    if (!voucher) {
+      return res.status(404).json({ message: 'Không tìm thấy Voucher' });
+    }
+
+    // Kiểm tra xem user đã sử dụng mã voucher này chưa
+    if (voucher.maxUse.includes(req.user.id)) {
+      return res.status(400).json({ message: 'Bạn đã sử dụng mã voucher này rồi!' });
+    }
+
+    // Cập nhật trường maxUse
+    voucher.maxUse.push(req.user.id);
+    await voucher.save();
+
+    return res.status(200).json({ data: voucher });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+},
   /* delete voucher */
   delete: async (req, res) => {
     try {
       const { id } = req.params;
       const voucher = await Voucher.findByIdAndDelete(id);
       if (!voucher) {
-        return res.status(404).json({ message: 'Không tìm thấy Voucher ' });
+        return res.status(404).json({ message: "Không tìm thấy Voucher " });
       }
       return res.status(200).json({ data: voucher });
     } catch (error) {
